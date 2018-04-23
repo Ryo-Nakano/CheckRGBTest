5890aa0e7b00c0aa4e0a19b5bacf435d606ce306
Create Unity Project <ryohei@R-NakanonoMacBook-Pro.local>
2018-04-23 13:08:08 +0900

    First

--- CheckRGBTest/ViewController.swift
+++ CheckRGBTest/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var rValue: UISlider!
+    @IBOutlet weak var gValue: UISlider!
+    @IBOutlet weak var bValue: UISlider!
+    var rInt: Int = 0
+    var gInt: Int = 0
+    var bInt: Int = 0
+    
+    @IBOutlet weak var rgbValueLabel: UILabel!//RGB値を表示する為のLabel
+    
+    //==========ライフサイクル==========
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //==========関数・メソッド==========
+    
+    //Sliderの変化と値の変化が同期させて処理！
+    
+    @IBAction func rSlider(_ sender: UISlider) {
+        print(Int(rValue.value))
+        //Slider側の設定をいじればOK！IntでSliderのValueを取れるようにはなった！
+        //あとはこれを背景色に反映させるだけ！
+        rInt = Int(rValue.value)
+        self.view.backgroundColor = changeColor(r: rInt, g: gInt, b: bInt)
     }
-
-
+    
+    @IBAction func gSlider(_ sender: UISlider) {
+        print(gValue.value)
+        gInt = Int(gValue.value)
+        self.view.backgroundColor = changeColor(r: rInt, g: gInt, b: bInt)
+    }
+    
+    @IBAction func bSlider(_ sender: UISlider) {
+        print(gValue.value)
+        bInt = Int(bValue.value)
+        self.view.backgroundColor = changeColor(r: rInt, g: gInt, b: bInt)
+    }
+    
+    //引数とって、UIColorを返す関数
+    func changeColor(r: Int, g: Int, b: Int) -> UIColor {
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1)
+    }
+    
 }
 
