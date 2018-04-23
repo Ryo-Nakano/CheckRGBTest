ba30f5221d41926d8b9a695f83fb644d5625369d
Create Unity Project <ryohei@R-NakanonoMacBook-Pro.local>
2018-04-23 13:49:39 +0900

    ・RGB値をスライダーの動きに合わせて追えるように変更

--- CheckRGBTest/ViewController.swift
+++ CheckRGBTest/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textLet: UILabel!
+    @IBOutlet weak var textRGB: UILabel!//RGB値を表示
+    //これ16進数のカラーコードで表示したいと思ってたんだけど、ちょっとよう分からんから保留！一旦！
+    
+    @IBOutlet weak var rValueLabel: UILabel!
+    @IBOutlet weak var gValueLabel: UILabel!
+    @IBOutlet weak var bValueLabel: UILabel!
     
     @IBOutlet weak var rValue: UISlider!
     @IBOutlet weak var gValue: UISlider!
@@ -25,12 +32,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        rInt = 127
+        gInt = 127
+        bInt = 127
+        self.view.backgroundColor = changeColor(r: rInt, g: gInt, b: bInt)
         
+        rValueLabel.text = String(rInt)//Rの値をLabelに反映
+        gValueLabel.text = String(gInt)//Gの値をLabelに反映
+        bValueLabel.text = String(bInt)//Rの値をLabelに反映
     }
     
     //==========関数・メソッド==========
     
-    //Sliderの変化と値の変化が同期させて処理！
+    //Sliderの変化と値の変化を同期させて処理！
     
     @IBAction func rSlider(_ sender: UISlider) {
         print(Int(rValue.value))
@@ -38,23 +52,56 @@ class ViewController: UIViewController {
         //あとはこれを背景色に反映させるだけ！
         rInt = Int(rValue.value)
         self.view.backgroundColor = changeColor(r: rInt, g: gInt, b: bInt)
+
+        rValueLabel.text = String(rInt)//Rの値をLabelに反映
+        
     }
     
     @IBAction func gSlider(_ sender: UISlider) {
         print(gValue.value)
         gInt = Int(gValue.value)
         self.view.backgroundColor = changeColor(r: rInt, g: gInt, b: bInt)
+        
+        gValueLabel.text = String(gInt)//Gの値をLabelに反映
     }
     
     @IBAction func bSlider(_ sender: UISlider) {
-        print(gValue.value)
+        print(bValue.value)
         bInt = Int(bValue.value)
         self.view.backgroundColor = changeColor(r: rInt, g: gInt, b: bInt)
+        
+        bValueLabel.text = String(bInt)//Rの値をLabelに反映
+        
+        changeTextColor()//bIntの値に応じて文字色を変更
     }
     
     //引数とって、UIColorを返す関数
     func changeColor(r: Int, g: Int, b: Int) -> UIColor {
         return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1)
+    }
+    
+    //背景色に応じてtextの色変える関数
+    func changeTextColor(){
+        if bInt > 127{
+            textLet.textColor = UIColor.black//文字色黒に変更
+            textRGB.textColor = UIColor.black//文字色黒に変更
+            rValueLabel.textColor = UIColor.black
+            gValueLabel.textColor = UIColor.black
+            bValueLabel.textColor = UIColor.black
+        }
+        else{
+            textLet.textColor = UIColor.white//文字色白に変更
+            textRGB.textColor = UIColor.white//文字色白に変更
+            rValueLabel.textColor = UIColor.white
+            gValueLabel.textColor = UIColor.white
+            bValueLabel.textColor = UIColor.white
+        }
+    }
+    
+    //UIColorを引数にとって、それをRGB値に変換する関数
+    func convertUIColorToRGB(_ color: UIColor) -> (red: Int, green: Int, blue: Int) {
+        let components = color.cgColor.components! // UIColorをCGColorに変換し、RGBとAlphaがそれぞれCGFloatで配列として取得できる
+        return (red: Int(components[0]), green: Int(components[1]), blue: Int(components[2]))
     }
     
 }
